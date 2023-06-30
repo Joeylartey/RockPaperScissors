1879eaf150411b7c5a0f41483ccddf7c680095fd
Jlartey <josephlarteyadu@gmail.com>
2023-06-30 11:23:56 +0000

    Made changes to the html, css and main.js files

--- main.js
+++ main.js
@@ -1,42 +1,299 @@
-const score = document.querySelector('.score-num');
-
-const choice1 = document.querySelector('.choice1');
-
-const choice2 = document.querySelector('.choice2');
-
-const choice3 = document.querySelector('.choice3');
-
-const imageTriangle = document.getElementById("image-triangle");
+let score = document.querySelector('.score-num');
+let initialScore = 0;
 
 const whoPickedWhat = document.querySelector(".who-picked-what");
-score.innerHTML = 30;
 
-choice1.addEventListener('click', () => {
-    choice1.style.display = "none";
-    choice2.style.display = "none";
-    choice3.style.display = "none";
-    imageTriangle.style.display = "none";
-    choice1.style.display = "block";
-    choice1.style.transform = "translate(-85px, 100px) scale(1.5)";
-    whoPickedWhat.style.display = "flex";
+const gameDiv = document.getElementById("game");
+
+gameDiv.className = 'game';
+
+const secondScreen = document.getElementById('second-screen');
+secondScreen.className = 'second-screen';
+
+const displayButton = document.createElement('button');
+
+// Create the image element with the id "image-triangle"
+const imageTriangle = document.createElement('img');
+imageTriangle.src = "./images/bg-triangle.svg";
+imageTriangle.alt = "Rock";
+imageTriangle.id = "image-triangle";
+gameDiv.appendChild(imageTriangle);
+
+// Create paperDiv
+const paperDiv = document.createElement('div');
+paperDiv.className = "paper";
+// paperDiv.id = "paperId"
+
+// Create the nested divs for choice1
+const thickBorderBlueDiv1 = document.createElement('div');
+thickBorderBlueDiv1.className = 'thick-border blue';
+
+const whiteBackgroundDiv1 = document.createElement('div');
+whiteBackgroundDiv1.className = "white-background";
+
+const paperImg = document.createElement('img');
+paperImg.src = './images/icon-paper.svg';
+paperImg.alt = 'Paper';
+paperImg.className = 'choice-image';
+
+whiteBackgroundDiv1.appendChild(paperImg);
+thickBorderBlueDiv1.appendChild(whiteBackgroundDiv1);
+paperDiv.appendChild(thickBorderBlueDiv1);
+gameDiv.appendChild(paperDiv);
+
+// Create scissorsDiv
+const scissorsDiv = document.createElement('div');
+scissorsDiv.className = 'scissors';
+
+// Create the nested divs for choice2
+const thickBorderOrangeDiv = document.createElement('div');
+thickBorderOrangeDiv.className = 'thick-border orange';
+
+const whiteBackgroundDiv2 =  document.createElement('div');
+whiteBackgroundDiv2.className = 'white-background';
+
+const scissorsImg = document.createElement('img');
+scissorsImg.src = './images/icon-scissors.svg';
+scissorsImg.alt = 'Scissors';
+scissorsImg.className = 'choice-image';
+
+whiteBackgroundDiv2.appendChild(scissorsImg);
+thickBorderOrangeDiv.appendChild(whiteBackgroundDiv2);
+scissorsDiv.appendChild(thickBorderOrangeDiv);
+gameDiv.appendChild(scissorsDiv);
+
+// Create rockDiv
+const rockDiv = document.createElement('div');
+rockDiv.className = 'rock';
+
+// Create the nested divs for choice3
+const thickBorderRedDiv = document.createElement('div');
+thickBorderRedDiv.className = 'thick-border red';
+
+const whiteBackgroundDiv3 = document.createElement('div');
+whiteBackgroundDiv3.className = 'white-background';
+
+const rockImg = document.createElement('img');
+rockImg.src = './images/icon-rock.svg';
+rockImg.alt = 'Rock';
+rockImg.className = 'choice-image';
+
+whiteBackgroundDiv3.appendChild(rockImg);
+thickBorderRedDiv.appendChild(whiteBackgroundDiv3);
+rockDiv.appendChild(thickBorderRedDiv);
+gameDiv.appendChild(rockDiv);
+
+// Add an click event listener to the paperDiv
+
+paperDiv.addEventListener('click', handlePaperClick);
+
+function handlePaperClick() {
+	
+	gameDiv.style.display = 'none';
+	whoPickedWhat.style.display = 'flex';
+	secondScreen.style.display = 'flex';
+	generatePaperDiv();
+
+	const displayResult = document.createElement('div');
+	displayResult.className = 'result-display';
+	let h2 = document.createElement('h2');
+	h2.innerHTML = '';
+	
+	displayButton.textContent = 'Play Again';
+	displayResult.appendChild(h2);
+	displayResult.appendChild(displayButton);
+	secondScreen.appendChild(displayResult);
+
+	const result = generateComputerDiv();	
+	if (result === 'rock') {
+		// console.log("You have won");
+		h2.innerHTML = 'You won';
+		score.innerHTML = initialScore + 1;
+		displayResult.style.display = 'flex';
+	} else if (result === 'scissors') {
+		// console.log('You have lost');
+		h2.innerHTML = 'You lost';
+		score.innerHTML = initialScore - 1;
+		displayResult.style.display = 'flex';
+	} else {
+		// console.log("There is a tie");
+		h2.innerHTML = 'There is a tie';
+		displayResult.style.display = 'flex';
+	}
+}
+
+scissorsDiv.addEventListener('click', () => {
+	gameDiv.style.display = 'none';
+	whoPickedWhat.style.display = 'flex';
+	secondScreen.style.display = 'flex';
+	generateScissorsDiv();
+
+	const displayResult = document.createElement('div');
+	displayResult.className = 'result-display';
+	let h2 = document.createElement('h2');
+	h2.innerHTML = '';
+	
+	displayButton.textContent = 'Play Again';
+	displayResult.appendChild(h2);
+	displayResult.appendChild(displayButton);
+	secondScreen.appendChild(displayResult);
+
+	const result = generateComputerDiv();
+	if (result === 'paper') {
+		// console.log("You have won");
+		h2.innerHTML = 'You won';
+		score.innerHTML = initialScore + 1;
+		displayResult.style.display = 'flex'
+	} else if (result === 'rock') {
+		// console.log('You have lost');
+		h2.innerHTML = 'You lost';
+		score.innerHTML = initialScore - 1;
+		displayResult.style.display = 'flex';
+	} else {
+		// console.log("There is a tie");
+		h2.innerHTML = 'There is a tie';
+		displayResult.style.display = 'flex';
+	}
 });
 
-choice2.addEventListener('click', () => {
-    choice1.style.display = "none";
-    choice2.style.display = "none";
-    choice3.style.display = "none";
-    imageTriangle.style.display = "none";
-    choice2.style.display = "block";
-    choice2.style.transform = "translate(-350px, 100px) scale(1.5)";
-    whoPickedWhat.style.display = "flex";
+rockDiv.addEventListener('click', () => {
+	gameDiv.style.display = 'none';
+	whoPickedWhat.style.display = 'flex';
+	secondScreen.style.display = 'flex';
+	generateRockDiv();
+
+	const displayResult = document.createElement('div');
+	displayResult.className = 'result-display';
+	let h2 = document.createElement('h2');
+	h2.innerHTML = '';
+	
+	displayButton.textContent = 'Play Again';
+	displayResult.appendChild(h2);
+	displayResult.appendChild(displayButton);
+	secondScreen.appendChild(displayResult);
+
+	const result = generateComputerDiv();
+	
+	if (result === 'scissors') {
+		// console.log("You have won");
+		h2.innerHTML = 'You won';
+		score.innerHTML = initialScore + 1;
+		displayResult.style.display = 'flex'
+	} else if (result === 'paper') {
+		// console.log("You have lost");
+		h2.innerHTML = 'You lost';
+		score.innerHTML = initialScore - 1;
+		displayResult.style.display = 'flex'
+	} else {
+		// console.log("There is a tie");
+		h2.innerHTML = 'There is a tie';
+		displayResult.style.display = 'flex'
+	}
 });
 
-choice3.addEventListener('click', () => {
-    choice1.style.display = "none";
-    choice2.style.display = "none";
-    choice3.style.display = "none";
-    imageTriangle.style.display = "none";
-    choice3.style.display = "block";
-    choice3.style.transform = "translate(-190px, -120px) scale(1.5)";
-    whoPickedWhat.style.display = "flex";
-})
+displayButton.addEventListener('click', () => {
+	console.log("Display Button has been clicked");
+	secondScreen.style.display = 'none';
+	whoPickedWhat.style.display = 'none';
+
+	gameDiv.style.display = 'block';
+
+});
+
+function generateComputerDiv() {
+	let randomNumber = Math.floor(Math.random() * 3)
+	let computerChoice;
+	switch (randomNumber) {
+		case 0: 
+			generateRockDiv();
+			computerChoice= 'rock'
+			break;
+		case 1:
+			generatePaperDiv();
+			computerChoice = 'paper';
+			break;
+			
+		case 2: 
+			generateScissorsDiv();
+			computerChoice = 'scissors';
+			break;
+	}
+	return computerChoice
+}
+
+function generatePaperDiv() {
+	// Generate paperDiv
+	const paperDiv = document.createElement('div');
+	// paperDiv.className = "second-screen";
+
+	// Create the nested divs for choice1
+	const thickBorderBlueDiv1 = document.createElement('div');
+	thickBorderBlueDiv1.className = 'thick-border blue';
+
+	const whiteBackgroundDiv1 = document.createElement('div');
+	whiteBackgroundDiv1.className = "white-background";
+
+	const paperImg = document.createElement('img');
+	paperImg.src = './images/icon-paper.svg';
+	paperImg.alt = 'Paper';
+	paperImg.className = 'choice-image';
+
+	whiteBackgroundDiv1.appendChild(paperImg);
+	thickBorderBlueDiv1.appendChild(whiteBackgroundDiv1);
+	paperDiv.appendChild(thickBorderBlueDiv1);
+	secondScreen.appendChild(paperDiv);
+	paperDiv.style.transform = 'scale(1.5)';
+
+}
+
+function generateScissorsDiv() {
+	// Generate scissorsDiv
+	const scissorsDiv = document.createElement('div');
+	// scissorsDiv.className = 'second-screen';
+
+	// Create the nested divs for choice2
+	const thickBorderOrangeDiv = document.createElement('div');
+	thickBorderOrangeDiv.className = 'thick-border orange';
+
+	const whiteBackgroundDiv2 =  document.createElement('div');
+	whiteBackgroundDiv2.className = 'white-background';
+
+	const scissorsImg = document.createElement('img');
+	scissorsImg.src = './images/icon-scissors.svg';
+	scissorsImg.alt = 'Scissors';
+	scissorsImg.className = 'choice-image';
+
+	whiteBackgroundDiv2.appendChild(scissorsImg);
+	thickBorderOrangeDiv.appendChild(whiteBackgroundDiv2);
+	scissorsDiv.appendChild(thickBorderOrangeDiv);
+	secondScreen.appendChild(scissorsDiv);
+	scissorsDiv.style.transform = 'scale(1.5)';
+}
+
+function generateRockDiv() {
+	// Generate rockDiv
+	const rockDiv = document.createElement('div');
+	// rockDiv.className = 'second-screen';
+
+	// Create the nested divs for choice3
+	const thickBorderRedDiv = document.createElement('div');
+	thickBorderRedDiv.className = 'thick-border red';
+
+	const whiteBackgroundDiv3 = document.createElement('div');
+	whiteBackgroundDiv3.className = 'white-background';
+
+	const rockImg = document.createElement('img');
+	rockImg.src = './images/icon-rock.svg';
+	rockImg.alt = 'Rock';
+	rockImg.className = 'choice-image';
+
+	whiteBackgroundDiv3.appendChild(rockImg);
+	thickBorderRedDiv.appendChild(whiteBackgroundDiv3);
+	rockDiv.appendChild(thickBorderRedDiv);
+
+	secondScreen.appendChild(rockDiv);
+	rockDiv.style.transform = 'scale(1.5)';
+}
+
+
+
